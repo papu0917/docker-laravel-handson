4095edec7bce3a4224722eff521a2806ba0e41c1
mitsuhiro <papu.m0917@icloud.com>
2021-03-20 21:59:21 +0900

    いらないコードの削除

--- backend/app/Models/Stock.php
+++ backend/app/Models/Stock.php
@@ -9,9 +9,4 @@ class Stock extends Model
     protected $guarded = [
         'id'
     ];
-
-    public function orders()
-    {
-        return $this->belongsToMany('App\Models\Order', 'order__xref_stock');
-    }
 }
