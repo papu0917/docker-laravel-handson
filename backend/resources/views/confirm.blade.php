d9f0ffa321809eb50f1fdbe9a69abc07f19ea2b9
mitsuhiro <papu.m0917@icloud.com>
2021-03-21 15:07:16 +0900

    ConfirmビューをCartInfoのDTOで表示できるようにした

--- backend/resources/views/confirm.blade.php
+++ backend/resources/views/confirm.blade.php
@@ -55,13 +55,13 @@
                     </thead>
 
                     <tbody>
-                        @foreach ($my_carts as $my_cart)
+                        @foreach ($cartInfo->carts() as $my_cart)
                             <tr>
                                 <th>{{ $my_cart->stock->name }}</th>
                                 <th>1</th>
                                 <th>{{ number_format($my_cart->stock->fee) }}円</th>
                                 <input type="hidden" name="stock_id[]" value="{{ $my_cart->stock->id }}">
-                                <input type="hidden" name="total_prices" value="{{ $sum }}">
+                                <input type="hidden" name="total_prices" value="{{ $cartInfo->sum() }}">
                                 <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                             </tr>
                         @endforeach
@@ -71,8 +71,8 @@
             </div>
         </div>
         <div class="text-center p-2">
-            個数：{{ $count }}個<br>
-            <p style="font-size:1.2em; font-weight:bold;">合計金額:{{ number_format($sum) }}円</p>
+            個数：{{ $cartInfo->count() }}個<br>
+            <p style="font-size:1.2em; font-weight:bold;">合計金額:{{ number_format($cartInfo->sum()) }}円</p>
         </div>
         @csrf
         <button type="submit" class="btn btn-danger btn-lg text-center buy-btn">購入する</button>
