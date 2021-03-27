bae1ea4e689abf40ac94d4045babd135601f98d1
mitsuhiro <papu.m0917@icloud.com>
2021-03-27 21:28:40 +0900

    注文履歴を全件表示できるようにした

--- backend/app/Http/Controllers/MypageController.php
+++ backend/app/Http/Controllers/MypageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
+use App\Models\Stock;
 
 
 class MypageController extends Controller
@@ -11,7 +13,11 @@ class MypageController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $ordersHistory = Order::where('user_id', $user->id);
+        $ordersHistory = Stock::whereHas('orders', function ($query) use ($ordersHistory) {
+            $ordersHistory->where('order_id', $ordersHistory);
+        })->get();
 
-        return view('/mypage', compact('user'));
+        return view('/mypage', compact('user', 'ordersHistory'));
     }
 }
