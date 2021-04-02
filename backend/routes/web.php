fb797cde1727476e1d4060d0c6b91586c834425d
mitsuhiro <papu.m0917@icloud.com>
2021-04-02 21:51:08 +0900

    vue.js、bladeのルーティングの設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
+
+
 Route::get('/', function () {
     logger('welcome route.');
     return view('shop');
